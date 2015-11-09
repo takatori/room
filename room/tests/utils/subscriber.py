f19b97d5f6b788bed41772db487843494227ac6e
takatori <takatori@ws.cs.kobe-u.ac.jp>
2015-11-09 21:05:18 +0900

    Move subscriber to package

---
+++ room/tests/utils/subscriber.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8- 
+
+import zmq
+import time
+
+class Subscriber():
+
+    def __init__(self, bind_addr, subscriber=b''):
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.SUB)
+        self.socket.connect('tcp://' + bind_addr)
+        self.socket.setsockopt(zmq.SUBSCRIBE, subscriber)
+        time.sleep(1.0)
+        
+    def recv(self):
+        return self.socket.recv_multipart()
+     
+
