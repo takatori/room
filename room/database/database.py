1bfb8053ade6434b6051ad923f90c4a49208de6a
takatori <takatori@ws.cs.kobe-u.ac.jp>
2015-11-09 15:27:18 +0900

    Add database module

---
+++ room/database/database.py
@@ -0,0 +1,56 @@
+#!/ur/bin/env python3
+# -*- coding: utf-8 -*-
+
+import time
+import zmq
+from abc import ABCMeta
+from abc import abstractmethod
+
+from room.utils import zmq_base as base
+
+class DatabaseModule(base.ZmqProcess):
+    
+    def __init__(self, bind_addr, database):
+        super().__init__()
+        self.bind_addr = bind_addr
+        self.sub_stream = None
+        self.db = database
+    
+    def setup():
+        super().setup()
+        self.sub_stream, _ = self.steam(zmq.SUB, self.bind_addr, bind=False, subscribe=keyword.encode('utf-8'))
+        self.stream.on_recv(SubStreamHandler(self.sub_stream, self.stop))
+
+    def run(self):
+        self.stop()
+        print('Start loop!')
+        self.loop.start()
+
+    def stop(self):
+        self.loop.stop()
+        
+class SubStreamHandler(base.MessageHandler):
+
+    def __init__(self, sub_stream, stop, db):
+        super().__init__()
+        self._sub_stream = sub_stream
+        self._stop = stop
+        self._db = db
+        
+    def mining(self, *data):
+        db.save(data)
+        
+    def stop(self, data):
+        self._stop()
+
+        
+class Database(metaclass=ABCMeta):
+
+    @abstractmethod
+    def save(self, data):
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def load(self):
+        raise NotImplementedError()        
+    
