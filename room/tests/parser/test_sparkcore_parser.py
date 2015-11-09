bf002c389b79698f047db5a2af6e2738d9e09e25
takatori <takatori@ws.cs.kobe-u.ac.jp>
2015-11-09 21:46:42 +0900

    Add test for sparkcore parser module

--- room/tests/parser/test_sparkcore_parser.py
+++ room/tests/parser/test_sparkcore_parser.py
@@ -4,8 +4,42 @@
 import unittest
 import json
 
-from room.parser.sparkcore_parser import SparkCoreParser
+from room.parser.sparkcore_parser import SparkCoreParserModule, SparkCoreParser
+from room.utils.publisher import Publisher
+from room.tests.utils.subscriber import Subscriber
 
+@unittest.skip("コルーチン化しないとテストできないっぽい")
+class SparkCoreParserModuleTest(unittest.TestCase)            :
+
+    @classmethod
+    def setUpClass(cls):
+        cls.publisher = Publisher("127.0.0.1:5558")
+        cls.sparkcore_parser = SparkCoreParserModule("127.0.0.1:5558")
+        cls.sparkcore_parser.run()
+        cls.subscriber = Subscriber('127.0.0.1:5557')
+
+    def test_sparkcore_parser_module(self):
+
+        data = """
+        ["sparkcore",{
+        "sensorId": "core04",
+        "temperature": 27.6,
+        "humidity": 41.200001,
+        "pressure": 991.455566,
+        "brightness": 34.848484,
+        "pir_count": 0
+        }]"""
+
+        self.publisher().send('sparkcore', 'parse', data)
+        result = self.subscribe.recv()
+        print(result)
+        self.assertTrue(True)
+
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDown!!!') 
+        cls.sparkcore_parser.stop()        
+        
 class SparkCoreParserTest(unittest.TestCase):
 
     def test_parser(self):
