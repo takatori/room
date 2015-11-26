c500109133db1be86075d248b6ace27cb141f652
takatori <takatori@ws.cs.kobe-u.ac.jp>
2015-11-26 17:12:21 +0900

    Add inout parser

---
+++ room/parser/inout_parser.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import parser_base
+
+from room.utils.config import config
+
+class InOutParserModule(parser_base.ParserModule):
+
+    def __init__(self, port):
+        super().__init__('localhost:{0}'.format(port), InOutParser())
+
+    def setup(self):
+        super().setup('inout')
+
+class InOutParser(parser_base.Parser):
+        
+    def parse(self, data):
+        data.pop('time')
+        return [('sensor', {key: data[key]}) for key in data.keys()]
+
+    
+if __name__ == "__main__":
+    proc = InOutParserModule(config['router_parser_forwarder']['back_port'])
+    proc.run()
+
+
+
+
+
+
+
+
+
