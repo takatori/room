30a46198b56bdc1fcd4d9f8532d71507804d9c69
takatori <takatori@ws.cs.kobe-u.ac.jp>
2015-11-26 17:38:40 +0900

    Ensure that inout output data type is json

--- room/parser/inout_parser.py
+++ room/parser/inout_parser.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import parser_base
+import json
 
 from room.utils.config import config
 
@@ -17,18 +18,11 @@ class InOutParser(parser_base.Parser):
         
     def parse(self, data):
         data.pop('time')
-        return [('sensor', {key: data[key]}) for key in data.keys()]
+        return [('sensor', json.dumps({key: data[key]})) for key in data.keys()]
 
     
 if __name__ == "__main__":
     proc = InOutParserModule(config['router_parser_forwarder']['back_port'])
     proc.run()
-
-
-
-
-
-
-
 
 
