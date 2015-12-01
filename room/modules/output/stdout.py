0da9dd56409eaea46f62d5f4437b373f1a631060
takatori <takatori@ws.cs.kobe-u.ac.jp>
2015-12-01 21:20:24 +0900

    Fix stdout module parameter

--- room/modules/output/stdout.py
+++ room/modules/output/stdout.py
@@ -6,7 +6,7 @@ from room.utils.config import network_config
 
 class StdOutModule(OutputModule):
 
-    def __init__(self, port):
+    def __init__(self):
         super().__init__(
             recv_addr='localhost:{0}'.format(network_config['forwarder5']['back']),            
             recv_title='',
