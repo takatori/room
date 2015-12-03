b3e4ab9b1b6a4d9ef3454a960801bf1f6ef35528
takatori <takatori@ws.cs.kobe-u.ac.jp>
2015-12-03 17:33:56 +0900

    Fix filter position

--- room/modules/database/mongo.py
+++ room/modules/database/mongo.py
@@ -11,7 +11,7 @@ class MongoModule(database.DatabaseModule):
 
     def __init__(self):
         super().__init__(
-            recv_addr='localhost:{0}'.format(network_config['forwarder3']['back']),
+            recv_addr='localhost:{0}'.format(network_config['forwarder4']['back']),
             recv_title=config['mongo']['keyword'],
             db=MongoDB()
         )
