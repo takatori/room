7b9285494b06d0f615acb74b96ea201d7e14ec29
takatori <takatori@ws.cs.kobe-u.ac.jp>
2016-01-22 21:41:23 +0900

    Add NNW module

---
+++ room/modules/miner/estimator/neuralnetwork.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import chainer
+from chainer import cuda, Function, gradient_check, Variable, optimizers, serializers, utils
+from chainer import Link, Chain, ChainList
+import chainer.functions as F
+import chainer.links as L
+
+
+
+
+if __name__ == '__main__':
+    pass
