b995de27a6e70f38ee0d9aabdc90da888efc3ff6
takatori <takatori@ws.cs.kobe-u.ac.jp>
2016-01-25 00:37:02 +0900

    Change recv port

--- room/modules/miner/nnw_ceilinglight_module.py
+++ room/modules/miner/nnw_ceilinglight_module.py
@@ -11,7 +11,7 @@ class NNWModule(MinerModule):
 
     def __init__(self):
         super().__init__(
-            recv_addr='localhost:{0}'.format(network_config['forwarder3']['back']),
+            recv_addr='localhost:{0}'.format(network_config['forwarder6']['back']),
             send_addr=int(network_config['forwarder5']['front']),
             recv_title='nnw',
             send_title='nnw',
