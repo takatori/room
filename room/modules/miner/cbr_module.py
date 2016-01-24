6a0f3282f8e414c75658e5de796346ba06e3a4f3
takatori <takatori@ws.cs.kobe-u.ac.jp>
2016-01-25 05:34:35 +0900

    Fix main method

--- room/modules/miner/cbr_module.py
+++ room/modules/miner/cbr_module.py
@@ -61,13 +61,6 @@ class MongoDB(object):
     
     
 if __name__ == "__main__":
-    #process = CBRModule()
-    #process.run()
-    
-    cbr = CBR()
-    data = {'timestamp': '2015-12-31 11:06:47.384507+09:00', 'appliances': {'fan': 0.0, 'ceilinglight': 0.0, 'floorlight': 0.0, 'viera': 0.0, 'curtain': 0.0, 'aircon': 0.0}, 'inout': {'arisa': 0.0, 'sakaki': 0.0, 'horihori': 0.0, 'shinsuke': 0.0, 'otokunaga': 0.0, 'tamamizu': 0.0, 'sachio': 0.0, 'masa-n': 0.0, 'takatori': 0.0, 'tabata': 0.0, 'inomoto': 0.0, 'tktk': 0.0, 'usk108': 1.0, 'junho': 0.0, 'masuda': 0.0, 'longniu': 0.0}, 'sensors': {'core02_brightness': 30.30303, 'core02_pressure': 1004.036011, 'core04_brightness': 10.606061, 'core04_temperature': 24.5, 'core01_pir count': 0.0, 'core01_temperature': 21.5, 'core02_humidity': 25.5, 'core01_pressure': 1009.623474, 'core04_pir count': 4.0, 'core04_pressure': 1005.958374, 'core02_pir count': 26.0, 'core01_brightness': 6.060606, 'core02_temperature': 24.0, 'core01_humidity': 20.0, 'core04_humidity': 45.0}}
-    start = time.time()
-    print(cbr.mining(data))
-    elapsed_time = time.time() - start
-    print("elapsed_time:{0}[sec]".format(elapsed_time))
+    process = CBRModule()
+    process.run()
     
